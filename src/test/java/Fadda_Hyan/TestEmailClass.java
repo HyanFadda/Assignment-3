23a60dd9e837b6b1dd216c7708cc7dd649747dc3
hyan fadda <hyanf@umich.edu>
2021-04-09 20:35:00 -0400

    Added TestEmailClass

--- src/test/java/Fadda_Hyan/TestEmailClass.java
+++ src/test/java/Fadda_Hyan/TestEmailClass.java
@@ -3,9 +3,13 @@ package Fadda_Hyan;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.Properties;
 
+import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
@@ -33,82 +37,4 @@ public class TestEmailClass {
 		assertEquals(60000, email.getSocketConnectionTimeout());
 	}
 
-	// This method is created to test the "getSentDate()" method
-	@Test
-	public void TestGetSentDate() {
-		Email email1 = new EmailMock();
-
-		email1.setSentDate(new Date());
-
-		assertEquals(new Date(), email1.getSentDate());
-
-		Email email2 = new EmailMock();
-		email2.setSentDate(null);
-
-		// It asserts that two Date objects are equal
-		assertEquals(new Date(), email2.getSentDate());
-	}
-
-	// This method is created to test the "addBcc(String... emails)" method
-	@Test
-	public void TestAddBcc() throws EmailException, AddressException {
-		Email email = new EmailMock();
-
-		String bccArr[] = new String[2];
-
-		bccArr[0] = new String("abc@example.com");
-		bccArr[1] = new String("xyz@example.com");
-
-		email.addBcc(bccArr);
-
-		assertEquals(new InternetAddress("abc@example.com"), email.getBccAddresses().get(0));
-		assertEquals(new InternetAddress("xyz@example.com"), email.getBccAddresses().get(1));
-
-		// It asserts that count of BCC addresses are equal
-		assertEquals(bccArr.length, email.getBccAddresses().size());
-	}
-
-	// This method is created to test the "addCc(String... emails)" method
-	@Test
-	public void TestAddcc() throws EmailException, AddressException {
-		Email email = new EmailMock();
-
-		String ccAddresses[] = new String[3];
-
-		ccAddresses[0] = new String("john@example.com");
-		ccAddresses[1] = new String("harry@example.com");
-		ccAddresses[2] = new String("roy@example.com");
-
-		email.addCc(ccAddresses);
-
-		assertEquals(new InternetAddress("john@example.com"), email.getCcAddresses().get(0));
-		assertEquals(new InternetAddress("harry@example.com"), email.getCcAddresses().get(1));
-		assertEquals(new InternetAddress("roy@example.com"), email.getCcAddresses().get(2));
-
-		// It asserts that count of CC addresses are equal
-		assertEquals(ccAddresses.length, email.getCcAddresses().size());
-
-	}
-
-	// This method is created to test the "setFrom(String email)" method
-	@Test
-	public void TestSetFrom() throws EmailException, AddressException {
-		Email email = new EmailMock();
-
-		email.setFrom("alex@example.com");
-
-		// It asserts that two From addresses are equal
-		assertEquals(new InternetAddress("alex@example.com"), email.getFromAddress());
-	}
-
-	// This method is created to test the "setHostName(String email)" method
-	@Test
-	public void TestGetHostName() throws EmailException, AddressException {
-		Email email = new EmailMock();
-
-		email.setHostName("vip.example.com");
-
-		// It asserts that two Host names are equal
-		assertEquals("vip.example.com", email.getHostName());
-	}
-}
+	
